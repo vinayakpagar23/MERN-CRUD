5b2cbff5571072c5fa6dd19aa2f22b292a397ffd
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-02-23 02:37:11 +0530

    model schema created

--- server/index.js
+++ server/index.js
@@ -2,16 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import router from './routes/route.js';
 
+import Routes from './routes/route.js';
 import Connection from './database/db.js';
 
 const app = express();
 
 
 dotenv.config();
+
+// To handle HTTP POST requests in Express.js version 4 and above, 
+// you need to install the middleware module called body-parser.
+// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
+app.use(bodyParser.json({extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/', router);
+
+app.use('/', Routes);
 
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;

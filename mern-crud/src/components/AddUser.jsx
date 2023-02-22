e51705d4c389c906ab5bfdc021bf71b99a62dd3a
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-02-23 00:05:28 +0530

    form created and working finenow woking on api call

--- mern-crud/src/components/AddUser.jsx
+++ mern-crud/src/components/AddUser.jsx
@@ -1,26 +1,55 @@
-import React from 'react'
-import { FormControl, FormGroup, InputLabel ,Input} from '@mui/material';
+import React, {useState} from 'react'
+import { FormControl, FormGroup, InputLabel ,Input, Typography,styled, Button} from '@mui/material';
+// import { useState } from 'react';
+
+const Container=styled(FormGroup)`
+width:50%;
+margin:5% auto 0 auto;
+&>div {
+    margin-top:20px;
+}
+`
+
+let defaultUser ={
+    name:"",
+    username:"",
+    email:"",
+    phone:""
+}
 const AddUser = () => {
+    const[user,setUser] = useState(defaultUser)
+    const onValueChnage=(e)=>{
+      setUser({...user,[e.target.name]:e.target.value})
+      console.log(user);
+    }
+
+    const addUserDetails=()=>{}
+
+    
   return (
     <>
-    <FormGroup>
+    <Container>
+    <Typography variant="h4">Add User</Typography>
         <FormControl>
             <InputLabel>Name</InputLabel>
-            <Input/>
+            <Input onChange={(e)=>onValueChnage(e)} name="name"/>
         </FormControl>
         <FormControl>
             <InputLabel>UserName</InputLabel>
-            <Input/>
+            <Input onChange={(e)=>onValueChnage(e)} name="username"/>
         </FormControl>
         <FormControl>
             <InputLabel>Email</InputLabel>
-            <Input/>
+            <Input onChange={(e)=>onValueChnage(e)} name="email"/>
         </FormControl>
         <FormControl>
             <InputLabel>Phone</InputLabel>
-            <Input/>
+            <Input onChange={(e)=>onValueChnage(e)} name="phone"/>
         </FormControl>
-    </FormGroup>
+        <FormControl>
+        <Button variant='contained' onClick={()=>addUserDetails}>AddUser</Button>
+        </FormControl>
+    </Container>
     </>
   )
 }

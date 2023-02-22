1d61369413205d8aa075dbbb10880b2e56d9832c
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-02-22 22:45:55 +0530

    working on navbar and material ui

---
+++ mern-crud/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AddUser from './components/AddUser';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <AddUser/>
+   </>
+  );
+}
+
+export default App;
---
+++ mern-crud/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+
+const Navbar = () => {
+  return (
+    <>
+    <AppBar>
+        <Toolbar>
+            
+        </Toolbar>
+    </AppBar>
+    </>
+  )
+}
+
+export default Navbar

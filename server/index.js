9bd19f3afa4db5d8062e36e6ff94558c47e8f92c
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-02-23 02:06:10 +0530

    backed router started

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import Connection from './database/db.js';
 const app = express();
+dotenv.config();
+
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
 const PORT = '8080';
 
-Connection();
+Connection(USERNAME, PASSWORD);
  
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
